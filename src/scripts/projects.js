1c9bfa55cc2ccbe34096f7f5047d7f10fbb8741d
acopperlily <april.copley@protonmail.com>
2024-02-24 12:33:46 -0500

    Create components for sections and imported images

---
+++ src/scripts/projects.js
@@ -0,0 +1,16 @@
+const projects = [
+  {
+    name: "Digital Canvas",
+    description: "Modeled after a beloved childhood toy, users employ simple mouse movements to transform a plain grid into a dazzling work of art.",
+  },
+  {
+    name: "Happy Hour Cocktails",
+    description: "Thirsty for something different? Users can search for drinks freshly served up by TheCocktailDB API. Tiny umbrellas not included.",
+  },
+  {
+    name: "Cats On Demand",
+    description: "Do you demand cats? Users can request cats freshly served up by Cataas (Cat as a Service) API.",
+  }
+];
+
+export default projects;
