539e965a56bf9eea0557d2a06e5eb1edfffc72f8
acopperlily <april.copley@protonmail.com>
2024-03-06 21:07:49 -0500

    Update header to include ARIA and move styles from global to local components

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,11 @@
-document.getElementById('menu').addEventListener('click', () => {
-  document.querySelector('.nav-links').classList.toggle('expanded');
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.btn--hamburger');
+  const nav = document.getElementById('navigation');
+
+  burger.addEventListener('click', function() {
+    const expanded = this.getAttribute('aria-expanded') === 'true' || false;
+    this.setAttribute('aria-expanded', String(!expanded));
+    nav.setAttribute('aria-hidden', String(expanded));
+    nav.classList.toggle('is-visible');
+  });
 });
