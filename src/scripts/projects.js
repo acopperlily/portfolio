95969b4a679bde5ac4eee72e3dc3ab37e493332d
acopperlily <april.copley@protonmail.com>
2024-03-18 21:00:17 -0400

    Update projects section

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -2,14 +2,20 @@ const projects = [
   {
     name: "Digital Canvas",
     description: "Modeled after a beloved childhood toy, users employ simple mouse movements to transform a plain grid into a dazzling work of art.",
+    pageLink: "https://acopperlily.github.io/digital-canvas/",
+    codeLink: "https://github.com/acopperlily/digital-canvas"
   },
   {
     name: "Happy Hour Cocktails",
     description: "Thirsty for something different? Users can search for drinks freshly served up by TheCocktailDB API. Tiny umbrellas not included.",
+    pageLink: "https://acopperlily.github.io/happy-hour-cocktails/",
+    codeLink: "https://github.com/acopperlily/happy-hour-cocktails"
   },
   {
     name: "Cats On Demand",
     description: "Do you demand cats? Users can request cats freshly served up by Cataas (Cat as a Service) API.",
+    pageLink: "https://acopperlily.github.io/cats-on-demand/",
+    codeLink: "https://github.com/acopperlily/cats-on-demand"
   }
 ];
 
