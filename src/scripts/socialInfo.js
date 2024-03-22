ab7a1784e766b58be0b6f9535edb0496959bb9cb
acopperlily <april.copley@protonmail.com>
2024-03-21 21:14:53 -0400

    Update contact section

---
+++ src/scripts/socialInfo.js
@@ -0,0 +1,19 @@
+const socialInfo = [
+  {
+    platform: 'Twitter',
+    url: 'https://twitter.com/aprilcopley_dev',
+    handle: '@aprilcopley_dev'
+  },
+  {
+    platform: 'GitHub',
+    url: 'https://github.com/acopperlily',
+    handle: 'github.com/acopperlily'
+  },
+  {
+    platform: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/april-copley/',
+    handle: 'linkedin.com/in/april-copley/'
+  }
+];
+
+export default socialInfo;
