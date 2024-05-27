e9e38ccb4fc994100a9841aa7311bf1bceac5315
acopperlily <april.copley@protonmail.com>
2024-05-27 16:44:31 -0400

    Parse links from paragraph text in project card info

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -3,19 +3,25 @@ const projects = [
     name: "Digital Canvas",
     description: "Modeled after a beloved childhood toy, users employ simple mouse movements to transform a plain grid into a dazzling work of art.",
     pageLink: "https://acopperlily.github.io/digital-canvas/",
-    codeLink: "https://github.com/acopperlily/digital-canvas"
+    codeLink: "https://github.com/acopperlily/digital-canvas",
+    apiLink: null,
+    linkText: null
   },
   {
     name: "Happy Hour Cocktails",
     description: "Thirsty for something different? Users can search for drinks freshly served up by TheCocktailDB API. Tiny umbrellas not included.",
     pageLink: "https://acopperlily.github.io/happy-hour-cocktails/",
-    codeLink: "https://github.com/acopperlily/happy-hour-cocktails"
+    codeLink: "https://github.com/acopperlily/happy-hour-cocktails",
+    apiLink: "https://www.thecocktaildb.com/api.php",
+    linkText: "TheCocktailDB"
   },
   {
     name: "Cats On Demand",
     description: "Do you demand cats? Users can request cats freshly served up by Cataas (Cat as a Service) API.",
     pageLink: "https://acopperlily.github.io/cats-on-demand/",
-    codeLink: "https://github.com/acopperlily/cats-on-demand"
+    codeLink: "https://github.com/acopperlily/cats-on-demand",
+    apiLink: "https://cataas.com/#/",
+    linkText: "Cataas"
   }
 ];
 
