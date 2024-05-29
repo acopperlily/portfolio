b0ee6dc72bfd94a995d507dc02236c6c6e0cdaea
acopperlily <april.copley@protonmail.com>
2024-05-29 14:24:56 -0400

    Add smooth scrolling, hover transformations, update descriptions

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -1,7 +1,7 @@
 const projects = [
   {
     name: "Digital Canvas",
-    description: "Modeled after a beloved childhood toy, users employ simple mouse movements to transform a plain grid into a dazzling work of art.",
+    description: "Modeled after a beloved childhood toy, simple hand movements can transform an empty grid into a dazzling work of art.",
     pageLink: "https://acopperlily.github.io/digital-canvas/",
     codeLink: "https://github.com/acopperlily/digital-canvas",
     apiLink: null,
@@ -9,7 +9,7 @@ const projects = [
   },
   {
     name: "Happy Hour Cocktails",
-    description: "Thirsty for something different? Users can search for drinks freshly served up by TheCocktailDB API. Tiny umbrellas not included.",
+    description: "Thirsty for something different? Search for drinks freshly served up by TheCocktailDB API. Tiny umbrellas not included.",
     pageLink: "https://acopperlily.github.io/happy-hour-cocktails/",
     codeLink: "https://github.com/acopperlily/happy-hour-cocktails",
     apiLink: "https://www.thecocktaildb.com/api.php",
@@ -17,7 +17,7 @@ const projects = [
   },
   {
     name: "Cats On Demand",
-    description: "Do you demand cats? Users can request cats freshly served up by Cataas (Cat as a Service) API.",
+    description: "Cats may dominate the internet, but so can you! Demand your dose of fun provided by Cataas (Cat as a Service) API.",
     pageLink: "https://acopperlily.github.io/cats-on-demand/",
     codeLink: "https://github.com/acopperlily/cats-on-demand",
     apiLink: "https://cataas.com/#/",
